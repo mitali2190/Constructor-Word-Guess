679377d5b74aea3b0d0d02ff1798ac6afbd4d616
Giorgi <lantareg@gmail.com>
2018-10-07 09:54:40 -0400

    init files

---
+++ index.js
@@ -0,0 +1,11 @@
+class index {
+    constructor(string, bool) {
+        this.string = string;
+        this.bool =bool;
+    }
+    getStr(){
+        if (bool) 
+        return string;
+    }
+    
+}
